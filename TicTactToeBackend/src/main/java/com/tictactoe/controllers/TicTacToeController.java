cf065953bc0833a4fbfae41d56229813553d556a
Michael <michaelkuang007@gmail.com>
2018-04-17 09:14:17 -0400

    blank sprint project

---
+++ TicTactToeBackend/src/main/java/com/tictactoe/controllers/TicTacToeController.java
@@ -0,0 +1,12 @@
+package com.tictactoe.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TicTacToeController {
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+}
